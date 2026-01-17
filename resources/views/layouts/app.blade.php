5b35414d878fcda65dfb64ca88320d3cb47e7679
Romazananda <romazzilasar@gmail.com>
2026-01-18 03:17:49 +0700

    Fix Vite entry point for Tailwind in production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Kaca Abadi')</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen flex flex-col bg-gray-100">
 
